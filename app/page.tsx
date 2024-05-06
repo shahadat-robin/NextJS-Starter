ef3248671103d6761b4052e6ce1aa46b7d653f96
Robin <connection.robin@gmail.com>
2024-05-06 13:05:48 +0600

    husky test

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ const HomePage = dynamic(() => import('@/page-builder/home-page'), {
   loading: () => <> Loading...</>,
 });
 
+const name = 'Robin';
+
 export default function Home() {
   return <HomePage />;
 }
