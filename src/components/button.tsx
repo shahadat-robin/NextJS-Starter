7bd9e158f4be42c9add00f0babfb31fc7a281cc1
Robin <connection.robin@gmail.com>
2024-05-06 14:12:44 +0600

    test with eslint and prettier

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,8 @@
 import React, { type PropsWithChildren, type FC } from 'react';
 
 const Button: FC<PropsWithChildren> = ({ children }) => {
+  console.log('Button component');
+
   return <button>{children}</button>;
 };
 
