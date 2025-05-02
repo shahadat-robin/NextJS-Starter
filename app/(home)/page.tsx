775991736cd4848dc19f6cba2459004ecd826dff
Robin <connection.robin@gmail.com>
2025-05-02 15:58:18 +0600

    bento grid layout implemented

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
 import AnotherSection from './sub-components/another-section';
+import CardGridLayoutSection from './sub-components/card-grid';
 import HeroSection from './sub-components/hero-section';
 
 const HomePage: NextPage = () => (
   <>
     <HeroSection />
-    <AnotherSection />
+    <CardGridLayoutSection />
     <AnotherSection />
     <AnotherSection />
   </>
---
+++ app/(home)/sub-components/card-grid/index.tsx
@@ -0,0 +1,36 @@
+import Container from '@/components/container';
+import { cn } from '@/utils/class-merge';
+
+export default function CardGridLayoutSection() {
+  const getColumnSpan = (index: number) => {
+    if (index % 9 === 2 || index % 9 === 4 || index % 9 === 6) {
+      return 'md:col-span-2 border-primary';
+    }
+    return 'col-span-1';
+  };
+
+  return (
+    <section className="min-h-screen shadow section-padding">
+      <Container className="grid md:grid-cols-4 gap-5">
+        {Array(9)
+          .fill('')
+          .map((_, index) => (
+            <div
+              key={index}
+              className={cn(
+                getColumnSpan(index),
+                'border p-5 rounded-md bg-white hover:shadow-md transition-shadow'
+              )}
+            >
+              <h3 className="text-gray-light text-5xl">{index + 1}</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque esse, accusamus
+                consectetur quia inventore laborum fuga saepe cum quidem soluta natus iste aperiam
+                non vero magni aspernatur nihil veniam cumque.
+              </p>
+            </div>
+          ))}
+      </Container>
+    </section>
+  );
+}
